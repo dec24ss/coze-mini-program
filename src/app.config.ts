0095baaabf02be62252d5bf1650e63b4830ca314
user8496815531 <3775053432629491-user8496815531@noreply.coze.cn>
2026-02-25 23:16:14 +0800

    auto saved your changes before deploy
    Coze-Commit-Type: auto

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,7 +2,8 @@ export default defineAppConfig({
   pages: [
     'pages/loading/index',
     'pages/index/index',
-    'pages/game/index'
+    'pages/game/index',
+    'pages/level-select/index'
   ],
   window: {
     backgroundTextStyle: 'light',
