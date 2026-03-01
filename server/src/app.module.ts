daf2a8a28a9037edb4df293d5297918d7f884be7
user8496815531 <3775053432629491-user8496815531@noreply.coze.cn>
2026-03-01 11:01:27 +0800

    Restored to '54471259e0ab31d112dc983176f14302532b5e1d'
    Coze-Restored-To: '54471259e0ab31d112dc983176f14302532b5e1d'
    Coze-Commit-Type: restore

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ImageModule } from '@/image/image.module';
 import { UsersModule } from '@/users/users.module';
-import { DesignPreviewModule } from '@/design-preview/design-preview.module';
 
 @Module({
-  imports: [ImageModule, UsersModule, DesignPreviewModule],
+  imports: [ImageModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/pages/design-preview/index.config.ts
+++
@@ -1,5 +0,0 @@
-export default typeof definePageConfig === 'function'
-  ? definePageConfig({
-      navigationBarTitleText: '设计预览'
-    })
-  : { navigationBarTitleText: '设计预览' }
